565e5c6f8d0bac48d424eb3d13419113fad1b7c7
Hailey Malueg <haileym44@gmail.com>
2026-05-26 07:47:42 -0600

    init:

---
+++ Loader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backtester
+{
+    internal class Loader
+    {
+        public Loader()
+        {
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Loader initialized and running...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿var program = new Program();
+program.Run();
+
+public partial class Program
+{
+    private Backtester.Loader _loader;
+
+    public Program()
+    {
+        _loader = new Backtester.Loader();
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Application started. Press Ctrl+C to exit.");
+        _loader.Start();
+
+        // Keep the program running until user stops it
+        while (true)
+        {
+            System.Threading.Thread.Sleep(100);
+        }
+    }
+}
